ea9a72faf3a0bef097364f2d5e84253b9890ba54
Elson Pang <elsonpang@Elsons-MacBook-Pro.local>
2019-11-26 15:54:01 +0800

    MPU CODE FOR Degree

---
+++ Debug/syscfg/ti_drivers_config.d
@@ -0,0 +1,91 @@
+# FIXED
+
+syscfg/ti_drivers_config.obj: syscfg/ti_drivers_config.c
+syscfg/ti_drivers_config.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stddef.h
+syscfg/ti_drivers_config.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_ti_config.h
+syscfg/ti_drivers_config.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/linkage.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/DeviceFamily.h
+syscfg/ti_drivers_config.obj: syscfg/ti_drivers_config.h
+syscfg/ti_drivers_config.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h
+syscfg/ti_drivers_config.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h
+syscfg/ti_drivers_config.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h
+syscfg/ti_drivers_config.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h
+syscfg/ti_drivers_config.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h
+syscfg/ti_drivers_config.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h
+syscfg/ti_drivers_config.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h
+syscfg/ti_drivers_config.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/I2C.h
+syscfg/ti_drivers_config.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/display/Display.h
+syscfg/ti_drivers_config.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdarg.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/display/DisplayUart.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/dpl/SemaphoreP.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/UART.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/i2c/I2CMSP432.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/dpl/HwiP.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/Power.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/utils/List.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/inc/msp.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/inc/msp432p401r.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/inc/msp_compatibility.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/inc/msp432p401r_classic.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/third_party/CMSIS/Include/core_cm4.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/third_party/CMSIS/Include/cmsis_version.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/third_party/CMSIS/Include/cmsis_compiler.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/third_party/CMSIS/Include/cmsis_ccs.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/third_party/CMSIS/Include/mpu_armv7.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/inc/system_msp432p401r.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/driverlib/interrupt.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/driverlib/i2c.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/driverlib/eusci.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/power/PowerMSP432.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/uart/UARTMSP432.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/dpl/ClockP.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/utils/RingBuf.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/driverlib/uart.h
+syscfg/ti_drivers_config.obj: /Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/Board.h
+
+syscfg/ti_drivers_config.c: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stddef.h: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_ti_config.h: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/linkage.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/DeviceFamily.h: 
+syscfg/ti_drivers_config.h: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/I2C.h: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/display/Display.h: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdarg.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/display/DisplayUart.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/dpl/SemaphoreP.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/UART.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/i2c/I2CMSP432.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/dpl/HwiP.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/Power.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/utils/List.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/inc/msp.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/inc/msp432p401r.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/inc/msp_compatibility.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/inc/msp432p401r_classic.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/third_party/CMSIS/Include/core_cm4.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/third_party/CMSIS/Include/cmsis_version.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/third_party/CMSIS/Include/cmsis_compiler.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/third_party/CMSIS/Include/cmsis_ccs.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/third_party/CMSIS/Include/mpu_armv7.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/inc/system_msp432p401r.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/driverlib/interrupt.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/driverlib/i2c.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/driverlib/eusci.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/power/PowerMSP432.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/uart/UARTMSP432.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/dpl/ClockP.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/utils/RingBuf.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/driverlib/uart.h: 
+/Users/elsonpang/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/Board.h: 
---
+++ i2cmasterexample1.c
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2016-2019, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ *    ======== i2cmasterexample1.c ========
+ */
+#include <stdint.h>
+#include <stddef.h>
+#include <unistd.h>
+
+#include <stdio.h>
+#include "msp432_mpu6050.h"
+#include "msp.h"
+/* DriverLib Includes */
+//#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+
+/*
+ *  ======== mainThread ========
+ */
+void *mainThread(void *arg0)
+{
+    printf("\n== Start of thread ==\n\n");
+    P1DIR |= BIT0;
+    P1OUT &= ~BIT0;
+    P6REN |= 0x30;
+    P6OUT |= 0x30;
+
+    printf("Startup MPU6050\n\n");
+    startMPU6050();
+    SysTick_init();
+    double periodTime = 20.38e-7;
+    //delayMs(5000);
+    //calibrateMPU6050();
+    printf("==================================\n");
+    printf("Reading MPU6050 Data...\n\n");
+
+        while(1)
+        {
+            P1OUT ^= BIT0;
+            //getGyroData();
+            // Gyro Data
+            int Start = SysTick->VAL;
+            int16_t* gyro = getGyro();
+            int End = SysTick->VAL;
+            int Delta = 0xFFFFFF & (Start - End);
+            int deltaComb = (periodTime * Delta)*1000;
+            int x = (gyro[0]/131)+4;
+            int y = (gyro[1]/131)-4;
+            int z = gyro[2]/131;
+//            printf("%.13f", Delta);
+//            printf("[Gyro]  X = %-4d, Y =  %-4d, Z = %-4d\n", (gyro[0]/131)+4, (gyro[1]/131)-4, gyro[2]/131);
+            int16_t* degree = getDegree(x,y,z,deltaComb);
+            printf("[degree]  X = %-4d, Y =  %-4d, Z = %-4d\n", degree[0], degree[1],degree[2]);
+
+            //Accel data
+            int16_t* accel = getAccel();
+//            printf("\n[Accel] X = %-4d, Y = %-4d, Z = %-4d\n", accel[0], accel[1], accel[2]);
+
+            int16_t* finalAngle = getFinalAngle(degree, accel);
+//            printf("\n[final] X = %-4d, Y = %-4d, Z = %-4d\n", finalAngle[0], finalAngle[1], finalAngle[2]);
+            printf("----------------------------------------\n");
+
+            delayMs(1000);
+        }
+    return (0);
+}
